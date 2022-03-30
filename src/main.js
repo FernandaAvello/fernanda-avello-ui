c7a98bf0f60d361ecc74677ff1210f80ccdef3f7
Fernanda Avello <fernandaavellorocha@gmail.com>
2022-03-30 19:11:47 -0300

    Inicio Proyecto Personal Site

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faTwitter)
+library.add(faInstagram)
+library.add(faLinkedin)
+library.add(faGithub)
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(BootstrapVue)
+
 
 Vue.config.productionTip = false
 
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
