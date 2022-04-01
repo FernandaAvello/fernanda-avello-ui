2a0eab63dcd59354823236b88ffb2aea129394e7
Fernanda Avello <fernandaavellorocha@gmail.com>
2022-04-01 17:51:27 -0300

    Import componentes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="home">
     <portada />
+    <section class="triangle2"></section>
+    <aboutMe />
+    <section class="triangle"></section>
+    <education />
+    <section class="triangle2"></section>
+
+    
   </div>
+  
 </template>
 
 <script>
-
-import portada from '@/components/portada.vue'
+import portada from "@/components/portada.vue";
+import aboutMe from "@/components/aboutMe.vue";
+import education from "@/components/education.vue";
 
 export default {
-  name: 'Home-view',
+  name: "Home-view",
   components: {
-    portada
-  }
-}
+    portada,
+    aboutMe,
+    education,
+  },
+};
 </script>
