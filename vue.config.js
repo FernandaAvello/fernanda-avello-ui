bc00a82f4122c8c07a4ebb2944ec887e31dde47f
facundovaras <facundovaras17@gmail.com>
2022-04-02 20:09:38 -0300

    cambiado el public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/fernanda-avello-ui/'
-    : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "fernanda-avello-ui" : "/",
 })
