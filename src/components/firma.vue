ee76e4f04a931423d10533b02b0a26c882861d0f
Fernanda Avello <fernandaavellorocha@gmail.com>
2022-04-02 21:34:37 -0300

    creacion component firma

---
+++ src/components/firma.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer id="footer">
+    <div class="col-lg-12">
+    <p class="copyright writeLato">© Copyright 2022 Fernanda Avello </p>
+  </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "firmaComponent",
+}
+</script>
+
+<style>
+#footer {
+  background-color: #46244c;
+  color: #fff;
+  padding-bottom: 0.5rem;
+  padding-top: 0.8rem;
+  text-align: center;
+}
+</style>
