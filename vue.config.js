f699b681205da6eb22fb2b60670f1d0ede0295d7
facundovaras <facundovaras17@gmail.com>
2022-04-02 21:27:33 -0300

    corrección de despliegue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "fernanda-avello-ui" : "/",
+  transpileDependencies: true
 })
